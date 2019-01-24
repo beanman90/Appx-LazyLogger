b35f754d9a7e32059a9e1ba97d278a8467fbd33c
BeanMan <domanh5790@segu>
2019-01-25 05:25:12 +0700

    Rename modules

--- app/src/main/java/beanman90/appx/lazylogger/demo/MainActivity.java
+++ app/src/main/java/beanman90/appx/lazylogger/demo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LazyLogger.plant(new LazyLogger.DebugTree());
+        LazyLogger.i("Hello Lazy Logger");
     }
 }
