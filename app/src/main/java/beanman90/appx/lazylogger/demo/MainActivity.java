05e489abfbb5fa4f1eb5a14b8018a6fcf3624c6d
BeanMan <domanh5790@segu>
2019-01-25 04:57:06 +0700

    Working fine! 1.0.0 Add proguard rules

--- app/src/main/java/beanman90/appx/lazylogger/demo/MainActivity.java
+++ app/src/main/java/beanman90/appx/lazylogger/demo/MainActivity.java
@@ -2,6 +2,7 @@ package beanman90.appx.lazylogger.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import beanman90.appx.lazylogger.LazyLogger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LazyLogger.plant(new LazyLogger.DebugTree());
     }
 }
